08de8051b7ac5c7fb767eff9a6cebc119f523238
Patrick McNulty <pamcnultyjr@gmail.com>
2020-02-04 17:11:59 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,25 +1,25 @@
 function basicTeenager(age) {
-  if (age >= 13 && age <= 19);{
-  return "You are a teenager";
+  if (age >= 13 && age <= 19) {
+  return ("You are a teenager");
 }
 }
 
 
 function teenager(age) {
-if (age >= 13 || age <= 19;){
-  return ("You are a teenager!")
-} else ("You are not")
+if (age >= 13 && age <= 19;){
+  return ("You are a teenager!");
+} else ("You are not");
 }
 
 function ageChecker(age) {
-  if (age >= 13 || age <= 19);{
-    ("You are a teenager!"){
+  if (age >= 13 && age <= 19); {
+    ("You are a teenager!"); {
 
-    }else if (age < 13) {
-    ("You are a kid"){
+    }else if (age <= 12) {
+    ("You are a kid"); {
 
-    }elseif (age > 19){
-    ("You are a grownup")
+    }else if (age >= 20){
+    ("You are a grownup");
 }
 
 function ternaryTeenager(age) {
