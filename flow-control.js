e2bcf481ea4c3f384ac0f4abd468a87452526509
Patrick McNulty <pamcnultyjr@gmail.com>
2020-02-04 18:53:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,3 +28,31 @@ function ternaryTeenager(age) {
   }
   return "You are not a teenager";
 }
+
+function switchAge(age) {
+  switch (age) {
+    case 13 :
+    return "You are a teenager";
+    break;
+    case 14 :
+    return "You are a teenager";
+    break;
+    case 15 :
+    return "You are a teenager";
+    break;
+    case 16 :
+    return "You are a teenager";
+    break;
+    case 17 :
+    return "You are a teenager";
+    break;
+    case 18 :
+    return "You are a teenager";
+    break;
+    case 19 :
+    return "You are a teenager";
+    break;
+    default :
+    return "You have an age"
+  }
+}
